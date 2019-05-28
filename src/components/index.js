ab412760f69f4b6235ce1c0e3f6f3855412051b4
ispoljari <ivanspoljaric22@gmail.com>
2019-05-28 13:32:06 +0200

    export all components through index.js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Board } from './board/Board';
+export { default as List } from './list/List';
+export { default as Card } from './card/Card';
+export { default as AddForm } from './add-form/AddForm';

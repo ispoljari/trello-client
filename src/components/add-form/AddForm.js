4a75e63deb15e2a99cd5fd7e57c7772cacd3fd07
ispoljari <ivanspoljaric22@gmail.com>
2019-05-28 13:38:33 +0200

    Create AddForm component

---
+++ src/components/add-form/AddForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import './add-form.css';
+
+export default class AddForm extends Component {
+  state = {
+    editing: false
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const text = this.textInput.value.trim();
+    if (text && this.props.onAdd) {
+      this.props.onAdd(text);
+    }
+    this.textInput.value = '';
+  }
+
+  setEditing = editing => {
+    this.setState({
+      editing
+    });
+  }
+
+  render() {
+    if (!this.state.editing) {
+      return (
+        <div className="add-button"
+          onClick={() => this.setEditing(true)}>
+          <a href="#">Add a {this.props.type}...</a>
+        </div>
+      );
+    }
+
+    return (
+      <form className="card add-form" onSubmit={(e) => this.onSubmit(e)}>
+        <input type="text" ref={input => this.textInput = input} />
+        <button>Add</button>
+        <button type="button" onClick={() => this.setEditing(false)}>
+          Cancel
+                </button>
+      </form>
+    );
+  }
+}

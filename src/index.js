fbf4d0a4a9bd6c472a0408b9881978ce6db23c65
ispoljari <ivanspoljaric22@gmail.com>
2019-05-28 13:09:33 +0200

    Import css global into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Board from './App';
+import Board from './Board';
 import './index.css';
 
 ReactDOM.render(<Board />, document.getElementById('root'));
